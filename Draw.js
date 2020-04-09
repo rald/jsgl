eb878488350f6da04dd525848253c84549bf01f3
rald <thecoconutnutisagiantnut@gmail.com>
2020-04-09 22:35:42 +0800

    update

--- Draw.js
+++ Draw.js
@@ -1,8 +1,46 @@
 var isDrawing = false;
+
+var colors = [
+  "black","white","red","orange","yellow","green","blue","indigo","violet"
+];
+
+var colorIndex=1;
+
+function inRect(x0,y0,x1,y1,w,h) {
+  return  x0>=x1 && x0<=x1+w &&
+          y0>=y1 && y0<=y1+h;
+}
+
 function render() {
+  
+  var size = 64;
+  
+  setLineWidth(1);  
+  setStroke("#ffffff");
+  setFill("#000000");
+  
+  cx=(canvas.width-colors.length*size)/2;
+  cy=4;
+  
+  fillRect(cx,cy,colors.length*size,size);
+  drawRect(cx,cy,colors.length*size,size);
+  
+  for(var i=0;i<colors.length;i++) {
+    var x=i*size+cx;
+    var y=cy;
+    setFill(colors[i]);
+    fillRect(x+8,y+8,size-16,size-16);
+    
+    if(inRect(mouseX,mouseY,x,y,size,size)) {
+      colorIndex=i;
+    }
+  }
+  setStroke("#ffffff");
+  drawRect(colorIndex*size+cx+4,cy+4,size-8,size-8);
+  
   if (isMouseDown) {
     if (isDrawing) {
-      setStroke("#ffffff");
+      setStroke(colors[colorIndex]);
       setLineWidth(4);
       drawLine(prevX, prevY, mouseX, mouseY);
       prevX = mouseX;
